ensenso/ros_driver

commit 9db1b00aea9188cc9f36b01bbedb2e2239a939e8

ros2: ensenso_camera_test: Use ros2 python compatibility layer

ensenso_camera_test/src/ensenso_camera_test/fit_primitive.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-import rospy
-import rostest
-
 import unittest
 
-import actionlib
-from actionlib_msgs.msg import GoalStatus
-from ensenso_camera_msgs.msg import FitPrimitiveAction, FitPrimitiveGoal, Primitive
-from ensenso_camera_msgs.msg import RequestDataAction, RequestDataGoal
+import ensenso_camera.ros2 as ros2py
 
-from helper import Point
+from ensenso_camera_msgs.msg import Primitive
+
+from ensenso_camera_test.helper import Point
+import ensenso_camera_test.ros2_testing as ros2py_testing
+
+FitPrimitive = ros2py.import_action("ensenso_camera_msgs", "FitPrimitive")
+RequestData = ros2py.import_action("ensenso_camera_msgs", "RequestData")
 
 INLIER_THRESHOLD = 0.005
 
@@ -48,6 +48,13 @@ def get_plane_fit_primitive():
     return primitive
 
 
+def format_error(error, note=None):
+    msg = "Error {}: {}".format(error.code, error.message)
+    if note:
+        msg = "{} ({})".format(note.strip(), msg)
+    return msg
+
+
 # Exact values of objects in the test scene
 class SphereTestValues:
     def __init__(self):
@@ -69,29 +76,33 @@ class PlaneTestValues:
 
 class TestFitPrimitive(unittest.TestCase):
     def setUp(self):
-        self.fit_primitive_client = actionlib.SimpleActionClient("fit_primitive", FitPrimitiveAction)
-        self.fit_primitive_client.wait_for_server()
+        self.node = ros2py.create_node("test_fit_primitive")
+        self.fit_primitive_client = ros2py.create_action_client(self.node, "fit_primitive", FitPrimitive)
+        self.request_data_client = ros2py.create_action_client(self.node, "request_data", RequestData)
 
-        self.request_data_client = actionlib.SimpleActionClient("request_data", RequestDataAction)
-        self.request_data_client.wait_for_server()
+        clients = [self.fit_primitive_client, self.request_data_client]
+        success = ros2py.wait_for_servers(self.node, clients, timeout_sec=ros2py_testing.TEST_TIMEOUT, exit=False)
+        self.assertTrue(success, msg="Timeout reached for servers.")
 
-        request_data_goal = RequestDataGoal()
+        request_data_goal = RequestData.Goal()
         request_data_goal.request_point_cloud = True
-        self.request_data_client.send_goal(request_data_goal)
-        self.request_data_client.wait_for_result()
+        response = ros2py.send_action_goal(self.node, self.request_data_client, request_data_goal)
 
-        result = self.request_data_client.get_result()
+        result = response.get_result()
         self.assertEqual(result.error.code, 0)
 
+    def tearDown(self):
+        ros2py.shutdown(self.node)
+
     def test_sphere(self):
-        goal = FitPrimitiveGoal()
+        goal = FitPrimitive.Goal()
         primitive = get_sphere_fit_primitive()
         goal.primitives.append(primitive)
         result = self.get_result(goal)
         self.result_check(result)
 
     def test_plane(self):
-        goal = FitPrimitiveGoal()
+        goal = FitPrimitive.Goal()
         primitive = get_plane_fit_primitive()
         goal.primitives.append(primitive)
 
@@ -99,7 +110,7 @@ class TestFitPrimitive(unittest.TestCase):
         self.result_check(result)
 
     def test_cylinder(self):
-        goal = FitPrimitiveGoal()
+        goal = FitPrimitive.Goal()
         primitive = get_cylinder_fit_primitive()
         goal.primitives.append(primitive)
 
@@ -126,17 +137,16 @@ class TestFitPrimitive(unittest.TestCase):
                 self.assertAlmostEqual(primitive.center.z, plane.center.z, delta=delta)
 
     def get_result(self, goal):
-        self.fit_primitive_client.send_goal(goal)
-        self.fit_primitive_client.wait_for_result()
-        self.assertEqual(self.fit_primitive_client.get_state(), GoalStatus.SUCCEEDED)
-        result = self.fit_primitive_client.get_result()
-        self.assertEqual(result.error.code, 0)
-        return result
+        response = ros2py.send_action_goal(self.node, self.fit_primitive_client, goal)
+        self.assertTrue(response.successful(), msg="Fit primitive action has not been successful.")
+        error = response.get_result().error
+        self.assertEqual(error.code, 0, msg=format_error(error, note="Fit primitive action exited with error!"))
+        return response.get_result()
+
+
+def main():
+    ros2py_testing.run_ros1_test("test_fit_primitive", TestFitPrimitive)
 
 
 if __name__ == "__main__":
-    try:
-        rospy.init_node("test_fit_primitive")
-        rostest.rosrun("ensenso_camera_test", "test_fit_primitive", TestFitPrimitive)
-    except rospy.ROSInterruptException:
-        pass
+    main()

ensenso_camera_test/src/ensenso_camera_test/colorized_point_cloud.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
-import rospy
-import rostest
-
 import unittest
-
-import actionlib
-
-from actionlib_msgs.msg import GoalStatus
-from ensenso_camera_msgs.msg import TexturedPointCloudAction, TexturedPointCloudGoal
-from ensenso_camera_msgs.msg import RequestDataMonoAction, RequestDataMonoGoal
-from ensenso_camera_msgs.msg import RequestDataAction, RequestDataGoal
 
 import ctypes
 import struct
 
-import sensor_msgs.point_cloud2 as pc2
+import ensenso_camera.ros2 as ros2py
 
-WAIT_TIMEOUT = 20
+import ensenso_camera_test.ros2_testing as ros2py_testing
+
+pc2 = ros2py_testing.import_point_cloud2()
+
+TexturedPointCloud = ros2py.import_action("ensenso_camera_msgs", "TexturedPointCloud")
+RequestDataMono = ros2py.import_action("ensenso_camera_msgs", "RequestDataMono")
+RequestData = ros2py.import_action("ensenso_camera_msgs", "RequestData")
 
 STEREO_NAMESPACE = "stereo"
 MONO_NAMESPACE = "mono"
@@ -25,14 +21,21 @@ MONO_SERIAL = "color_mono"
 
 class TestColorizedPointCloud(unittest.TestCase):
     def setUp(self):
-        self.request_stereo = actionlib.SimpleActionClient(STEREO_NAMESPACE + "/request_data", RequestDataAction)
-        self.request_mono = actionlib.SimpleActionClient(MONO_NAMESPACE + "/request_data", RequestDataMonoAction)
-        self.colorize_point_cloud = actionlib.SimpleActionClient(
-            STEREO_NAMESPACE + "/texture_point_cloud", TexturedPointCloudAction
+        self.node = ros2py.create_node("test_colorized_point_cloud")
+        self.request_stereo_client = ros2py.create_action_client(
+            self.node, STEREO_NAMESPACE + "/request_data", RequestData
         )
-        for client in [self.request_mono, self.request_stereo, self.colorize_point_cloud]:
-            if not client.wait_for_server(rospy.Duration(WAIT_TIMEOUT)):
-                self.fail(msg="Timeout reached for servers.")
+        self.request_mono_client = ros2py.create_action_client(
+            self.node, MONO_NAMESPACE + "/request_data", RequestDataMono
+        )
+        self.colorize_point_cloud_client = ros2py.create_action_client(
+            self.node, STEREO_NAMESPACE + "/texture_point_cloud", TexturedPointCloud
+        )
+
+        clients = [self.request_stereo_client, self.request_mono_client, self.colorize_point_cloud_client]
+        success = ros2py.wait_for_servers(self.node, clients, timeout_sec=ros2py_testing.TEST_TIMEOUT, exit=False)
+        self.assertTrue(success, msg="Timeout reached for servers.")
+        self.colored_point_cloud = None
 
     def test_colorize_point_cloud(self):
         self.send_data_goals()
@@ -41,36 +44,43 @@ class TestColorizedPointCloud(unittest.TestCase):
         self.check_color()
 
     def send_data_goals(self):
-        goal_mono = RequestDataMonoGoal()
+        goal_stereo = RequestData.Goal()
+        goal_stereo.request_point_cloud = True
+        goal_mono = RequestDataMono.Goal()
         goal_mono.request_rectified_images = True
-        goal = RequestDataGoal()
-        goal.request_point_cloud = True
-        self.request_mono.send_goal(goal_mono)
-        self.request_stereo.send_goal(goal)
 
-        for client in [self.request_mono, self.request_stereo]:
-            if not client.wait_for_result(rospy.Duration(WAIT_TIMEOUT)):
-                self.fail(msg="Timeout reached for results.")
-            self.assertTrue(client.get_state() == GoalStatus.SUCCEEDED, msg="Request action has not been successful.")
+        goals = [goal_stereo, goal_mono]
+        clients = [self.request_stereo_client, self.request_mono_client]
+        stereo_response, mono_response = ros2py.send_action_goals(self.node, clients, goals)
+
+        self.assertFalse(stereo_response.timeout(), msg="Timeout reached for stereo results.")
+        self.assertFalse(mono_response.timeout(), msg="Timeout reached for mono results.")
+
+        self.assertTrue(stereo_response.successful(), msg="Request stereo data action has not been successful.")
+        self.assertTrue(mono_response.successful(), msg="Request mono data action has not been successful.")
 
     def aquire_data(self):
-        goal_texture = TexturedPointCloudGoal()
+        goal_texture = TexturedPointCloud.Goal()
         goal_texture.use_opengl = False
         goal_texture.include_results_in_response = True
         goal_texture.mono_serial = MONO_SERIAL
         goal_texture.far_plane = 4000.0
         goal_texture.near_plane = 100.0
-        self.colorize_point_cloud.send_goal(goal_texture)
-        if not self.colorize_point_cloud.wait_for_result(rospy.Duration(WAIT_TIMEOUT)):
-            self.fail(msg="Timeout reached for results.")
+
+        response = ros2py.send_action_goal(self.node, self.colorize_point_cloud_client, goal_texture)
+
+        self.assertFalse(response.timeout(), msg="Timeout reached for results.")
+        self.assertTrue(response.successful(), msg="Request textured point cloud action has not been successful.")
+
+        self.colored_point_cloud = response.get_result()
 
     def recieved_point_cloud(self):
-        result = self.colorize_point_cloud.get_result()
+        result = self.colored_point_cloud
         self.assertEqual(result.error.message, "", msg="Got an error with the result")
         self.cloud_points = list(pc2.read_points(result.point_cloud, skip_nans=True))
         self.assertTrue(
             len(self.cloud_points) != 0,
-            msg=" The recieved point cloud is empty. Recieved {} points".format(len(self.cloud_points)),
+            msg="The recieved point cloud is empty. Recieved {} points".format(len(self.cloud_points)),
         )
 
     def check_color(self):
@@ -93,9 +103,9 @@ class TestColorizedPointCloud(unittest.TestCase):
             self.assertTrue(b != 0.0, msg="b-value cannot be zero")
 
 
+def main():
+    ros2py_testing.run_ros1_test("test_colorized_point_cloud", TestColorizedPointCloud)
+
+
 if __name__ == "__main__":
-    try:
-        rospy.init_node("test_colorized_point_cloud")
-        rostest.rosrun("ensenso_camera_test", "test_colorized_point_cloud", TestColorizedPointCloud)
-    except rospy.ROSInterruptException:
-        pass
+    main()

ensenso_camera_test/src/ensenso_camera_test/hand_eye_calibration.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
-import rospy
-import rostest
-
 import glob
 import os
 import unittest
 
-import actionlib
-import tf
+import ensenso_camera.ros2 as ros2py
 
-from actionlib_msgs.msg import GoalStatus
-from ensenso_camera_msgs.msg import CalibrateHandEyeAction, CalibrateHandEyeGoal
+from ensenso_camera_test.helper import Pose
+import ensenso_camera_test.ros2_testing as ros2py_testing
 
-from helper import Pose
+CalibrateHandEye = ros2py.import_action("ensenso_camera_msgs", "CalibrateHandEye")
 
-DATA_SET_DIRECTORY = "../data/hand_eye_calibration/"
+DATA_SET_DIRECTORY = ros2py_testing.get_test_data_path("hand_eye_calibration/")
 
 ROBOT_BASE_FRAME = "robot_base"
 ROBOT_WRIST_FRAME = "robot_wrist"
@@ -25,31 +21,34 @@ FRAMES_WITHOUT_PATTERNS = [2, 3]
 
 class TestHandEyeCalibration(unittest.TestCase):
     def setUp(self):
-        self.tf_broadcaster = tf.TransformBroadcaster()
+        self.node = ros2py.create_node("test_hand_eye_calibration")
+        self.tf_broadcaster = ros2py_testing.create_tf_broadcaster(self.node)
+        self.calibration_client = ros2py.create_action_client(self.node, "calibrate_hand_eye", CalibrateHandEye)
+        success = ros2py.wait_for_server(
+            self.node, self.calibration_client, timeout_sec=ros2py_testing.TEST_TIMEOUT, exit=False
+        )
+        self.assertTrue(success, msg="Timeout reached for servers.")
 
-        self.calibration_client = actionlib.SimpleActionClient("calibrate_hand_eye", CalibrateHandEyeAction)
-        self.calibration_client.wait_for_server()
+    def assert_goal_response(self, response, goal):
+        result = response.get_result()
+        self.assertTrue(response.successful())
+        self.assertEqual(result.error.code, 0)
+        self.assertEqual(result.command, goal.command)
 
-    def assert_goal_successful(self):
-        self.assertEqual(self.calibration_client.get_state(), GoalStatus.SUCCEEDED)
-        self.assertEqual(self.calibration_client.get_result().error.code, 0)
-
-    def send_calibration_goal(self, goal, feedback_cb=None, wait=True):
-        self.calibration_client.send_goal(goal, feedback_cb=feedback_cb)
-
-        if wait:
-            self.calibration_client.wait_for_result()
-            self.assert_goal_successful()
-            self.assertEqual(self.calibration_client.get_result().command, goal.command)
+    def send_calibration_goal(self, goal, feedback_callback=None):
+        response = ros2py.send_action_goal(self.node, self.calibration_client, goal, feedback_callback)
+        self.assert_goal_response(response, goal)
+        return response
 
     def _broadcast_tf(self, pose, child_frame, parent_frame):
-        self.tf_broadcaster.sendTransform(pose.position, pose.orientation, rospy.Time.now(), child_frame, parent_frame)
+        timestamp = self.node.get_clock().now()
+        ros2py_testing.send_tf_transform(self.tf_broadcaster, pose, timestamp, child_frame, parent_frame)
 
     def test_hand_eye_calibration(self):
-        self.send_calibration_goal(CalibrateHandEyeGoal(command=CalibrateHandEyeGoal.RESET))
+        response = self.send_calibration_goal(CalibrateHandEye.Goal(command=CalibrateHandEye.Goal.RESET))
 
-        result = self.calibration_client.get_result()
-        self.assertEqual(result.command, CalibrateHandEyeGoal.RESET)
+        result = response.get_result()
+        self.assertEqual(result.command, CalibrateHandEye.Goal.RESET)
         self.assertEqual(result.error_message, "")
         self.assertEqual(result.error.code, 0)
 
@@ -59,10 +58,10 @@ class TestHandEyeCalibration(unittest.TestCase):
             current_robot_pose = Pose.from_json(link_file)
 
             self._broadcast_tf(current_robot_pose, ROBOT_WRIST_FRAME, ROBOT_BASE_FRAME)
-            self.send_calibration_goal(CalibrateHandEyeGoal(command=CalibrateHandEyeGoal.CAPTURE_PATTERN))
+            response = self.send_calibration_goal(CalibrateHandEye.Goal(command=CalibrateHandEye.Goal.CAPTURE_PATTERN))
 
-            result = self.calibration_client.get_result()
-            self.assertEqual(result.command, CalibrateHandEyeGoal.CAPTURE_PATTERN)
+            result = response.get_result()
+            self.assertEqual(result.command, CalibrateHandEye.Goal.CAPTURE_PATTERN)
             self.assertEqual(result.error_message, "")
             self.assertEqual(result.error.code, 0)
 
@@ -84,19 +83,19 @@ class TestHandEyeCalibration(unittest.TestCase):
 
         self.last_feedback = None
 
+        @ros2py_testing.feedback_callback
         def on_feedback(feedback):
             self.last_feedback = feedback
 
-        self.send_calibration_goal(
-            CalibrateHandEyeGoal(command=CalibrateHandEyeGoal.START_CALIBRATION), feedback_cb=on_feedback
-        )
+        goal = CalibrateHandEye.Goal(command=CalibrateHandEye.Goal.START_CALIBRATION)
+        response = self.send_calibration_goal(goal, feedback_callback=on_feedback)
 
         self.assertIsNotNone(self.last_feedback)
         self.assertNotEqual(self.last_feedback.number_of_iterations, 0)
         self.assertNotEqual(self.last_feedback.residual, 0)
 
-        result = self.calibration_client.get_result()
-        self.assertEqual(result.command, CalibrateHandEyeGoal.START_CALIBRATION)
+        result = response.get_result()
+        self.assertEqual(result.command, CalibrateHandEye.Goal.START_CALIBRATION)
         self.assertEqual(result.error_message, "")
         self.assertEqual(result.error.code, 0)
 
@@ -110,9 +109,9 @@ class TestHandEyeCalibration(unittest.TestCase):
         self.assertTrue(final_pattern_pose.equals(Pose.from_message(result.pattern_pose)))
 
 
+def main():
+    ros2py_testing.run_ros1_test("test_hand_eye_calibration", TestHandEyeCalibration)
+
+
 if __name__ == "__main__":
-    try:
-        rospy.init_node("test_hand_eye_calibration")
-        rostest.rosrun("ensenso_camera_test", "test_hand_eye_calibration", TestHandEyeCalibration)
-    except rospy.ROSInterruptException:
-        pass
+    main()

ensenso_camera_test/src/ensenso_camera_test/helper.py
@@ -1,7 +1,15 @@
 import json
 import math
 
-import tf
+from ensenso_camera_test.ros2_testing import import_tf_transformation_function
+
+quaternion_about_axis = import_tf_transformation_function("quaternion_about_axis")
+translation_matrix = import_tf_transformation_function("translation_matrix")
+quaternion_matrix = import_tf_transformation_function("quaternion_matrix")
+concatenate_matrices = import_tf_transformation_function("concatenate_matrices")
+inverse_matrix = import_tf_transformation_function("inverse_matrix")
+translation_from_matrix = import_tf_transformation_function("translation_from_matrix")
+quaternion_from_matrix = import_tf_transformation_function("quaternion_from_matrix")
 
 
 class Pose:
@@ -22,7 +30,7 @@ class Pose:
             transform["Translation"][2] / 1000,
         ]
         axis = (transform["Rotation"]["Axis"][0], transform["Rotation"]["Axis"][1], transform["Rotation"]["Axis"][2])
-        orientation = tf.transformations.quaternion_about_axis(transform["Rotation"]["Angle"], axis)
+        orientation = quaternion_about_axis(transform["Rotation"]["Angle"], axis)
 
         return cls(position, orientation)
 
@@ -37,13 +45,13 @@ class Pose:
         """
         Get the inverse of the pose.
         """
-        translation = tf.transformations.translation_matrix(self.position)
-        rotation = tf.transformations.quaternion_matrix(self.orientation)
-        transform = tf.transformations.concatenate_matrices(translation, rotation)
+        translation = translation_matrix(self.position)
+        rotation = quaternion_matrix(self.orientation)
+        transform = concatenate_matrices(translation, rotation)
 
-        inverse_transform = tf.transformations.inverse_matrix(transform)
-        translation = tf.transformations.translation_from_matrix(inverse_transform)
-        rotation = tf.transformations.quaternion_from_matrix(inverse_transform)
+        inverse_transform = inverse_matrix(transform)
+        translation = translation_from_matrix(inverse_transform)
+        rotation = quaternion_from_matrix(inverse_transform)
 
         return Pose(translation, rotation)
 
@@ -104,13 +112,13 @@ class RegionOfInterest:
         return cls(lower, upper)
 
     def write_to_message(self, message):
-        message.lower.x = self.lower[0]
-        message.lower.y = self.lower[1]
-        message.lower.z = self.lower[2]
+        message.lower.x = float(self.lower[0])
+        message.lower.y = float(self.lower[1])
+        message.lower.z = float(self.lower[2])
 
-        message.upper.x = self.upper[0]
-        message.upper.y = self.upper[1]
-        message.upper.z = self.upper[2]
+        message.upper.x = float(self.upper[0])
+        message.upper.y = float(self.upper[1])
+        message.upper.z = float(self.upper[2])
 
     def is_empty(self):
         for lower, upper in zip(self.lower, self.upper):

ensenso_camera_test/src/ensenso_camera_test/locate_multiple_patterns.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
-import rospy
-import rostest
-
 import unittest
 
-import actionlib
-from actionlib_msgs.msg import GoalStatus
-from ensenso_camera_msgs.msg import LocatePatternAction, LocatePatternGoal
+import ensenso_camera.ros2 as ros2py
+
+import ensenso_camera_test.ros2_testing as ros2py_testing
+
+LocatePattern = ros2py.import_action("ensenso_camera_msgs", "LocatePattern")
 
 
-class TestLocatePattern(unittest.TestCase):
+class TestLocateMultiplePatterns(unittest.TestCase):
     def setUp(self):
-        self.locate_pattern_client = actionlib.SimpleActionClient("locate_pattern", LocatePatternAction)
-        self.locate_pattern_client.wait_for_server()
+        self.node = ros2py.create_node("test_locate_multiple_patterns")
+        self.locate_pattern_client = ros2py.create_action_client(self.node, "locate_pattern", LocatePattern)
+        ros2py.wait_for_server(self.node, self.locate_pattern_client)
 
     def test_locate_multiple_patterns(self):
-        self.locate_pattern_client.send_goal(LocatePatternGoal())
-        self.locate_pattern_client.wait_for_result()
-        self.assertEqual(self.locate_pattern_client.get_state(), GoalStatus.SUCCEEDED)
-        result = self.locate_pattern_client.get_result()
+        response = ros2py.send_action_goal(self.node, self.locate_pattern_client, LocatePattern.Goal())
+        result = response.get_result()
+
+        self.assertTrue(response.successful())
         self.assertEqual(result.error.code, 0)
 
         self.assertTrue(result.found_pattern)
@@ -26,9 +26,9 @@ class TestLocatePattern(unittest.TestCase):
         self.assertEqual(len(result.pattern_poses), 2)
 
 
+def main():
+    ros2py_testing.run_ros1_test("test_locate_multiple_patterns", TestLocateMultiplePatterns)
+
+
 if __name__ == "__main__":
-    try:
-        rospy.init_node("test_locate_multiple_patterns")
-        rostest.rosrun("ensenso_camera_test", "test_locate_multiple_patterns", TestLocatePattern)
-    except rospy.ROSInterruptException:
-        pass
+    main()

ensenso_camera_test/src/ensenso_camera_test/locate_pattern.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python
-import rospy
-import rostest
-
-import os
 import unittest
 
-import actionlib
-from actionlib_msgs.msg import GoalStatus
-from ensenso_camera_msgs.msg import LocatePatternAction, LocatePatternGoal
+import ensenso_camera.ros2 as ros2py
 
-from helper import Pose
+from ensenso_camera_test.helper import Pose
+import ensenso_camera_test.ros2_testing as ros2py_testing
 
-DATA_SET_DIRECTORY = "../data/locate_pattern/"
+LocatePattern = ros2py.import_action("ensenso_camera_msgs", "LocatePattern")
+
+DATA_SET_FILEPATH = ros2py_testing.get_test_data_path("locate_pattern/pattern_pose.json")
 
 FRAMES_CONTAIN_PATTERN = [True, False]
 
 
 class TestLocatePattern(unittest.TestCase):
     def setUp(self):
-        self.locate_pattern_client = actionlib.SimpleActionClient("locate_pattern", LocatePatternAction)
-        self.locate_pattern_client.wait_for_server()
+        self.node = ros2py.create_node("test_locate_pattern")
+        self.locate_pattern_client = ros2py.create_action_client(self.node, "locate_pattern", LocatePattern)
+        ros2py.wait_for_server(self.node, self.locate_pattern_client)
 
     def test_locate_pattern(self):
         for contains_pattern in FRAMES_CONTAIN_PATTERN:
-            self.locate_pattern_client.send_goal(LocatePatternGoal())
-            self.locate_pattern_client.wait_for_result()
-            self.assertEqual(self.locate_pattern_client.get_state(), GoalStatus.SUCCEEDED)
-            result = self.locate_pattern_client.get_result()
+            response = ros2py.send_action_goal(self.node, self.locate_pattern_client, LocatePattern.Goal())
+            result = response.get_result()
+
+            self.assertTrue(response.successful())
             self.assertEqual(result.error.code, 0)
 
             if contains_pattern:
@@ -47,7 +45,7 @@ class TestLocatePattern(unittest.TestCase):
                 self.assertNotEqual(result.pattern_poses[0].header.stamp, 0)
                 self.assertNotEqual(result.pattern_poses[0].header.frame_id, "")
 
-                reference_pattern_pose = Pose.from_json(os.path.join(DATA_SET_DIRECTORY, "pattern_pose.json"))
+                reference_pattern_pose = Pose.from_json(DATA_SET_FILEPATH)
                 estimated_pattern_pose = Pose.from_message(result.pattern_poses[0].pose)
                 self.assertTrue(reference_pattern_pose.equals(estimated_pattern_pose))
             else:
@@ -56,9 +54,9 @@ class TestLocatePattern(unittest.TestCase):
                 self.assertEqual(len(result.pattern_poses), 0)
 
 
+def main():
+    ros2py_testing.run_ros1_test("test_locate_pattern", TestLocatePattern)
+
+
 if __name__ == "__main__":
-    try:
-        rospy.init_node("test_locate_pattern")
-        rostest.rosrun("ensenso_camera_test", "test_locate_pattern", TestLocatePattern)
-    except rospy.ROSInterruptException:
-        pass
+    main()

ensenso_camera_test/src/ensenso_camera_test/locate_pattern_mono.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python
-import rospy
-import rostest
-
-import os
 import unittest
 
-import actionlib
-from actionlib_msgs.msg import GoalStatus
-from ensenso_camera_msgs.msg import LocatePatternMonoAction, LocatePatternMonoGoal
+import ensenso_camera.ros2 as ros2py
 
-from helper import Pose
+from ensenso_camera_test.helper import Pose
+import ensenso_camera_test.ros2_testing as ros2py_testing
 
-DATA_SET_DIRECTORY = "../data/locate_pattern_mono/"
+LocatePatternMono = ros2py.import_action("ensenso_camera_msgs", "LocatePatternMono")
+
+DATA_SET_FILEPATH = ros2py_testing.get_test_data_path("locate_pattern_mono/pattern_pose.json")
 
 
-class TestLocatePattern(unittest.TestCase):
+class TestLocatePatternMono(unittest.TestCase):
     def setUp(self):
-        self.locate_pattern_client = actionlib.SimpleActionClient("locate_pattern", LocatePatternMonoAction)
-        self.locate_pattern_client.wait_for_server()
+        self.node = ros2py.create_node("test_locate_pattern_mono")
+        self.locate_pattern_client = ros2py.create_action_client(self.node, "locate_pattern", LocatePatternMono)
+        ros2py.wait_for_server(self.node, self.locate_pattern_client)
 
     def test_locate_pattern(self):
-        self.locate_pattern_client.send_goal(LocatePatternMonoGoal())
-        self.locate_pattern_client.wait_for_result()
-        self.assertEqual(self.locate_pattern_client.get_state(), GoalStatus.SUCCEEDED)
-        result = self.locate_pattern_client.get_result()
+        response = ros2py.send_action_goal(self.node, self.locate_pattern_client, LocatePatternMono.Goal())
+        result = response.get_result()
 
+        self.assertTrue(response.successful())
         self.assertEqual(result.error.code, 0)
 
         self.assertTrue(result.found_pattern)
@@ -43,14 +40,14 @@ class TestLocatePattern(unittest.TestCase):
         self.assertNotEqual(result.mono_pattern_poses[0].header.stamp, 0)
         self.assertNotEqual(result.mono_pattern_poses[0].header.frame_id, "")
 
-        reference_pattern_pose = Pose.from_json(os.path.join(DATA_SET_DIRECTORY, "pattern_pose.json"))
+        reference_pattern_pose = Pose.from_json(DATA_SET_FILEPATH)
         estimated_pattern_pose = Pose.from_message(result.mono_pattern_poses[0].pose)
         self.assertTrue(reference_pattern_pose.equals(estimated_pattern_pose))
 
 
+def main():
+    ros2py_testing.run_ros1_test("test_locate_pattern_mono", TestLocatePatternMono)
+
+
 if __name__ == "__main__":
-    try:
-        rospy.init_node("test_locate_pattern_mono")
-        rostest.rosrun("ensenso_camera_test", "test_locate_pattern_mono", TestLocatePattern)
-    except rospy.ROSInterruptException:
-        pass
+    main()

belalhmedan90/ensenso_ros_driver

commit e4b1cc42bf0854b3027757fe9deb37f649980952

ensenso_camera: scripts: Fix error formatting
Broken in 9db1b00aea9188cc9f36b01bbedb2e2239a939e8.

ensenso_camera/src/ensenso_camera/ros2.py
@@ -23,6 +23,13 @@ def is_ros2():
     return os.environ["ROS_VERSION"] == "2"
 
 
+def format_error(error, note=None):
+    msg = "Error {}: {}".format(error.code, error.message)
+    if note:
+        msg = "{} ({})".format(note.strip(), msg)
+    return msg
+
+
 # ----------------------------------------------------------------------------------------------------------------------
 # ROS2
 # ----------------------------------------------------------------------------------------------------------------------

ensenso_camera_test/src/ensenso_camera_test/fit_primitive.py
@@ -48,13 +48,6 @@ def get_plane_fit_primitive():
     return primitive
 
 
-def format_error(error, note=None):
-    msg = "Error {}: {}".format(error.code, error.message)
-    if note:
-        msg = "{} ({})".format(note.strip(), msg)
-    return msg
-
-
 # Exact values of objects in the test scene
 class SphereTestValues:
     def __init__(self):
@@ -140,7 +133,7 @@ class TestFitPrimitive(unittest.TestCase):
         response = ros2py.send_action_goal(self.node, self.fit_primitive_client, goal)
         self.assertTrue(response.successful(), msg="Fit primitive action has not been successful.")
         error = response.get_result().error
-        self.assertEqual(error.code, 0, msg=format_error(error, note="Fit primitive action exited with error!"))
+        self.assertEqual(error.code, 0, msg=ros2py.format_error(error, note="Fit primitive action exited with error!"))
         return response.get_result()
 
 